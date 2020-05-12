cddf47fde7a0116ada9eae63b108ea713de09a13
ashikmahmud12345@gmail.com <ashikmahmud12345@gmail.com>
2020-05-12 11:15:24 +0600

    seventh commit

--- userPanel/assets/js/main.js
+++ userPanel/assets/js/main.js
@@ -6,3 +6,30 @@ $(".custom-file-input").on("change", function () {
 $(document).ready(function () {
     $("input[maxlength]").maxlength();
 });
+
+
+function writeFunctionDes() {
+    document.getElementById("textArea").readOnly = "false";
+  }
+  function previewFunctionDes() {
+    document.getElementById("textArea").readOnly = "true";
+  }
+
+  function writeFunctionRep() {
+    document.getElementById("reproduce").readOnly = "false";
+  }
+  function previewFunctionRep() {
+    document.getElementById("reproduce").readOnly = "true";
+  }
+  function writeFunctionExp() {
+    document.getElementById("exploitation").readOnly = "false";
+  }
+  function previewFunctionExp() {
+    document.getElementById("exploitation").readOnly = "true";
+  }
+  function writeFunctionFix() {
+    document.getElementById("fixation")= "true";
+  }
+  function previewFunctionFix() {
+    document.getElementById("fixation").readOnly = "true";
+  }
